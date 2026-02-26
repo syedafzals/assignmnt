8fa593b2d365c787e07b7326d65e3159da0eb20b
syedafzals <syedafzals110@gmail.com>
2026-02-26 06:27:59 +0000

    coment

---
+++ task.js/Project.js/task.js
@@ -0,0 +1,9 @@
+let studentname=Ali;
+let totalmarks= 500;
+let obtainedmarks=425;
+
+let percntage=(obtainedmarks/ totalmarks)
+console.log("studentname:", studentname)
+console.log("totalmarks:", totalmarks)
+console.log("obtainedmarks;", obtainedmarks)
+console.log("percntage:", percntage+ "%")
