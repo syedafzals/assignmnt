eeb4f0b7edf49e45c5b46ec39a151a0ff304211b
syedafzals <syedafzals110@gmail.com>
2026-02-26 06:52:24 +0000

    project1

---
+++ project/basic01.js
@@ -0,0 +1,18 @@
+let studentname="Ali";
+let totalmarks= 500;
+let obtainedmarks=425;
+
+let percantage=(obtainedmarks / totalmarks)
+console.log("studentname:" , studentname)
+console.log("totalmarks:" , totalmarks)
+console.log("obtainedmarks:" , obtainedmarks)
+console.log("percantage:" , percantage + "%")
+
+const id=3565;
+let name="Afzal";
+let age=12;
+let batch=2021;
+let country="pakistan";
+console.table(["id " + id, "Afzal ", "age " + age,"batch "+ batch, "country "+ country,] )
+
+
--- task.js/Project.js/task.js
+++
@@ -1,9 +0,0 @@
-let studentname=Ali;
-let totalmarks= 500;
-let obtainedmarks=425;
-
-let percntage=(obtainedmarks/ totalmarks)
-console.log("studentname:", studentname)
-console.log("totalmarks:", totalmarks)
-console.log("obtainedmarks;", obtainedmarks)
-console.log("percntage:", percntage+ "%")
