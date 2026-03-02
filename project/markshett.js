8b5e26859038c9932be5f0dc268394bebc5cc484
syedafzals <syedafzals110@gmail.com>
2026-03-02 06:48:42 +0000

    comnt

---
+++ project/markshett.js
@@ -0,0 +1,25 @@
+let name="Syed Afzal Shah";
+console.log("name :" + name)
+let rollnum="0221";
+console.log("rollnum :" + rollnum)
+// subject
+let Sindhi=33;
+let Urdu=66;
+let Maths=50;
+let Chemistry=67;
+let Biology=54;
+let totalmarks=(Sindhi+Urdu+Maths+Chemistry+Biology);
+
+let Grade="B";
+let percantage=(totalmarks/ 680)*100;
+
+console.log("Sindhi :"+ Sindhi)
+console.log("Urdu :" + Urdu)
+console.log("Chemistry :" + Chemistry)
+console.log("Maths :" + Maths)
+console.log("Biology :" + Biology)
+console.log("Grade :"+ Grade)
+console.log("percantage :" + percantage)
+console.log(" totalmarks:" + totalmarks)
+
+
